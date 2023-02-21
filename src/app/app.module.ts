24c63c62306ae2c9b038202e19cd3f000a0ea95f
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-02-21 10:46:37 +0100

    cmsB-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,11 @@
+export class Contact {
+
+  constructor ( public id: string, public name: string, public email: string, public phone: string, public imageUrl: string, public group: Contact[]) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+      this.imageUrl = imageUrl;
+      this.group = group;
+  }
+}
