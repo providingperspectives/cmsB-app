dc77d46ee726fbd0805bdf444c8a233cffcfbfe7
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-03-15 17:57:02 +0100

    Fixed-nodejs

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use('/documents', documentRoutes);
 
 // Tell express to map all other non-defined routes back to the index page
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/cmsb/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/cms-b-app/index.html'));
 });
 
 // Define the port address and tell express to use this port
